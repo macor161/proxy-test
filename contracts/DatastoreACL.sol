c33939184d3117e5bd231401d91114753cda52a3
Mathew Cormier <mathew.corm@gmail.com>
2018-10-12 09:37:48 -0400

    Initial commit

---
+++ contracts/DatastoreACL.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+import "@aragon/os/contracts/apps/AragonApp.sol";
+
+
+contract DatastoreACL is AragonApp {
+
+    function test() public returns (uint) {
+        return 42;
+    }
+
+}
